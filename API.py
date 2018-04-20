08982359530f4f72c65d6465c6d0bf3d60fbe531
ztex030640417@gmail.com <ztex030640417@gmail.com>
2018-04-20 21:56:19 +0800

    nop

---
+++ API.py
@@ -0,0 +1,18 @@
+import cognitive_face as CF
+
+KEY = '<Subscription Key>'  # Replace with a valid subscription key (keeping the quotes in place).
+CF.Key.set(KEY)
+
+BASE_URL = 'https://westus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
+CF.BaseUrl.set(BASE_URL)
+
+# You can use this example JPG or replace the URL below with your own URL to a JPEG image.
+img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+faces = CF.face.detect(img_url)
+print(faces)
+
+
+"""
+return : 
+[{u'faceId': u'68a0f8cf-9dba-4a25-afb3-f9cdf57cca51', u'faceRectangle': {u'width': 89, u'top': 66, u'height': 89, u'left': 446}}]
+"""
